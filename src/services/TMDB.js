0782ea12986c34d81235066d22d1a97a1fc65762
Isaac Khan <isaackhan21@gmail.com>
2022-07-25 17:07:23 +0100

    Actor page

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -45,9 +45,25 @@ export const tmdbApi = createApi({
     //* get User Specific Lists
     getRecommendations: builder.query({
       query: ({movie_id, list}) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`
+    }),
+
+    getActorsDetails: builder.query({
+      query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
+    }),
+
+    getMoviesByActorId: builder.query({
+      query: ({id, page}) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`
     })
 
   }),
 });
 
-export const { useGetGenresQuery, useGetMoviesQuery, useGetMovieQuery, useGetRecommendationsQuery} = tmdbApi;
+export const { 
+        useGetGenresQuery, 
+        useGetMoviesQuery, 
+        useGetMovieQuery, 
+        useGetRecommendationsQuery, 
+        useGetActorsDetailsQuery,
+        useGetMoviesByActorIdQuery,
+
+      } = tmdbApi;
