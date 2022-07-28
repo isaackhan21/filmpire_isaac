68ccf3bac4630bd616ead9d69f018cef189b7671
Isaac Khan <isaackhan21@gmail.com>
2022-07-28 17:47:43 +0100

    Featured Movie Card

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Box,
   CircularProgress,
@@ -6,10 +6,10 @@ import {
   Typography,
 } from "@mui/material";
 import { useSelector } from "react-redux";
-import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
+
 
 import { useGetMoviesQuery } from "../../services/TMDB";
-import { MovieList, Pagination } from "..";
+import { FeaturedMovie, MovieList, Pagination } from "..";
 
 const Movies = () => {
   const [page, setPage] = useState(1);
@@ -23,7 +23,7 @@ const Movies = () => {
   });
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
-  const numberOfMovies = lg ? 16 : 18;
+  const numberOfMovies = lg ? 17 : 19;
 
   if (isFetching) {
     return (
@@ -48,7 +48,8 @@ const Movies = () => {
   if (error) return "An error has ocurred.";
   return (
     <div>
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <FeaturedMovie movie={data.results[0]}/>
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages}/>
     </div>
   );
